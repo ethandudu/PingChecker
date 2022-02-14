ba581508afe41b24d6fc8b3a89d90e7b505cba9a
Ethan <41381482+ethandudu@users.noreply.github.com>
2022-02-14 13:47:20 +0100

    V2
    Added Stats
    Fixed ping on local network return an Error
    Added an Export button

--- PingChecker/Main.Designer.vb
+++ PingChecker/Main.Designer.vb
@@ -34,7 +34,6 @@ Partial Class Main
         Me.Latency = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Start_BT = New System.Windows.Forms.Button()
-        Me.Stop_BT = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Name_LB = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
@@ -43,8 +42,26 @@ Partial Class Main
         Me.Started_time_LB = New System.Windows.Forms.Label()
         Me.Credit = New System.Windows.Forms.Label()
         Me.Version_LB = New System.Windows.Forms.Label()
+        Me.Export_BT = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Time_MLB = New System.Windows.Forms.Label()
+        Me.Avg_MLB = New System.Windows.Forms.Label()
+        Me.Max_MLB = New System.Windows.Forms.Label()
+        Me.Min_MLB = New System.Windows.Forms.Label()
+        Me.Rcv_MLB = New System.Windows.Forms.Label()
+        Me.Lost_MLB = New System.Windows.Forms.Label()
+        Me.Sent_MLB = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Icon_PB, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
@@ -55,7 +72,7 @@ Partial Class Main
         Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
         DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
-        DataGridViewCellStyle1.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
         DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
         DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
         DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
@@ -65,18 +82,18 @@ Partial Class Main
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ID, Me.Time, Me.Domain, Me.Latency})
         DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
-        DataGridViewCellStyle2.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
         DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
         DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
         DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
         DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
         Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle2
-        Me.DataGridView1.Location = New System.Drawing.Point(144, 111)
+        Me.DataGridView1.Location = New System.Drawing.Point(27, 111)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
         DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control
-        DataGridViewCellStyle3.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
         DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText
         DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight
         DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
@@ -91,76 +108,65 @@ Partial Class Main
         Me.ID.HeaderText = "ID"
         Me.ID.Name = "ID"
         Me.ID.ReadOnly = True
-        Me.ID.Width = 46
+        Me.ID.Width = 43
         '
         'Time
         '
         Me.Time.HeaderText = "Date"
         Me.Time.Name = "Time"
         Me.Time.ReadOnly = True
-        Me.Time.Width = 59
+        Me.Time.Width = 55
         '
         'Domain
         '
         Me.Domain.HeaderText = "Domain"
         Me.Domain.Name = "Domain"
         Me.Domain.ReadOnly = True
-        Me.Domain.Width = 76
+        Me.Domain.Width = 68
         '
         'Latency
         '
         Me.Latency.HeaderText = "Latency"
         Me.Latency.Name = "Latency"
         Me.Latency.ReadOnly = True
-        Me.Latency.Width = 76
+        Me.Latency.Width = 70
         '
         'TextBox1
         '
-        Me.TextBox1.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
-        Me.TextBox1.Location = New System.Drawing.Point(350, 375)
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.TextBox1.Location = New System.Drawing.Point(345, 375)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.Size = New System.Drawing.Size(110, 20)
         Me.TextBox1.TabIndex = 1
         '
         'Start_BT
         '
         Me.Start_BT.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Start_BT.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
-        Me.Start_BT.Location = New System.Drawing.Point(322, 401)
+        Me.Start_BT.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Start_BT.Location = New System.Drawing.Point(363, 401)
         Me.Start_BT.Name = "Start_BT"
         Me.Start_BT.Size = New System.Drawing.Size(75, 23)
         Me.Start_BT.TabIndex = 2
         Me.Start_BT.Text = "Start"
         Me.Start_BT.UseVisualStyleBackColor = True
         '
-        'Stop_BT
-        '
-        Me.Stop_BT.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Stop_BT.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
-        Me.Stop_BT.Location = New System.Drawing.Point(403, 401)
-        Me.Stop_BT.Name = "Stop_BT"
-        Me.Stop_BT.Size = New System.Drawing.Size(75, 23)
-        Me.Stop_BT.TabIndex = 3
-        Me.Stop_BT.Text = "Stop"
-        Me.Stop_BT.UseVisualStyleBackColor = True
-        '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!)
-        Me.Label1.Location = New System.Drawing.Point(360, 350)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Label1.Location = New System.Drawing.Point(367, 350)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(80, 15)
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Destination :"
         '
         'Name_LB
         '
         Me.Name_LB.AutoSize = True
-        Me.Name_LB.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!)
+        Me.Name_LB.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
         Me.Name_LB.Location = New System.Drawing.Point(341, 9)
         Me.Name_LB.Name = "Name_LB"
-        Me.Name_LB.Size = New System.Drawing.Size(127, 24)
+        Me.Name_LB.Size = New System.Drawing.Size(119, 24)
         Me.Name_LB.TabIndex = 4
         Me.Name_LB.Text = "PingChecker"
         '
@@ -181,20 +187,20 @@ Partial Class Main
         'Start_LB
         '
         Me.Start_LB.AutoSize = True
-        Me.Start_LB.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Start_LB.Location = New System.Drawing.Point(294, 311)
+        Me.Start_LB.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Start_LB.Location = New System.Drawing.Point(177, 311)
         Me.Start_LB.Name = "Start_LB"
-        Me.Start_LB.Size = New System.Drawing.Size(77, 15)
+        Me.Start_LB.Size = New System.Drawing.Size(62, 13)
         Me.Start_LB.TabIndex = 3
         Me.Start_LB.Text = "Started at : "
         '
         'Started_time_LB
         '
         Me.Started_time_LB.AutoSize = True
-        Me.Started_time_LB.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Started_time_LB.Location = New System.Drawing.Point(364, 311)
+        Me.Started_time_LB.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Started_time_LB.Location = New System.Drawing.Point(247, 311)
         Me.Started_time_LB.Name = "Started_time_LB"
-        Me.Started_time_LB.Size = New System.Drawing.Size(143, 15)
+        Me.Started_time_LB.Size = New System.Drawing.Size(124, 13)
         Me.Started_time_LB.TabIndex = 3
         Me.Started_time_LB.Text = "XX/XX/XXXX XX:XX:XX"
         '
@@ -202,17 +208,17 @@ Partial Class Main
         '
         Me.Credit.AutoSize = True
         Me.Credit.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Credit.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Credit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Credit.Location = New System.Drawing.Point(12, 426)
         Me.Credit.Name = "Credit"
-        Me.Credit.Size = New System.Drawing.Size(165, 15)
+        Me.Credit.Size = New System.Drawing.Size(140, 13)
         Me.Credit.TabIndex = 3
         Me.Credit.Text = "Made with 💖 by Ethandudu"
         '
         'Version_LB
         '
         Me.Version_LB.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Version_LB.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Version_LB.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Version_LB.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Version_LB.Location = New System.Drawing.Point(725, 426)
         Me.Version_LB.Name = "Version_LB"
@@ -221,11 +227,179 @@ Partial Class Main
         Me.Version_LB.Text = "X.X.X.X"
         Me.Version_LB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
+        'Export_BT
+        '
+        Me.Export_BT.Location = New System.Drawing.Point(464, 314)
+        Me.Export_BT.Name = "Export_BT"
+        Me.Export_BT.Size = New System.Drawing.Size(75, 23)
+        Me.Export_BT.TabIndex = 6
+        Me.Export_BT.Text = "Export"
+        Me.Export_BT.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.CheckFileExists = True
+        Me.SaveFileDialog1.DefaultExt = "csv"
+        Me.SaveFileDialog1.Filter = "CSV File|*.csv"
+        Me.SaveFileDialog1.RestoreDirectory = True
+        Me.SaveFileDialog1.Title = "Choose path for exporting"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Time_MLB)
+        Me.GroupBox1.Controls.Add(Me.Avg_MLB)
+        Me.GroupBox1.Controls.Add(Me.Max_MLB)
+        Me.GroupBox1.Controls.Add(Me.Min_MLB)
+        Me.GroupBox1.Controls.Add(Me.Rcv_MLB)
+        Me.GroupBox1.Controls.Add(Me.Lost_MLB)
+        Me.GroupBox1.Controls.Add(Me.Sent_MLB)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(588, 167)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 116)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Stats"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(6, 98)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(76, 13)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Time elapsed :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 85)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Avg :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 72)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(33, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Max :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 59)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Min :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 46)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Received :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 33)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Lost :"
+        '
+        'Time_MLB
+        '
+        Me.Time_MLB.AutoSize = True
+        Me.Time_MLB.Location = New System.Drawing.Point(88, 98)
+        Me.Time_MLB.Name = "Time_MLB"
+        Me.Time_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Time_MLB.TabIndex = 0
+        Me.Time_MLB.Text = "XXX"
+        '
+        'Avg_MLB
+        '
+        Me.Avg_MLB.AutoSize = True
+        Me.Avg_MLB.Location = New System.Drawing.Point(37, 85)
+        Me.Avg_MLB.Name = "Avg_MLB"
+        Me.Avg_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Avg_MLB.TabIndex = 0
+        Me.Avg_MLB.Text = "XXX"
+        '
+        'Max_MLB
+        '
+        Me.Max_MLB.AutoSize = True
+        Me.Max_MLB.Location = New System.Drawing.Point(37, 72)
+        Me.Max_MLB.Name = "Max_MLB"
+        Me.Max_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Max_MLB.TabIndex = 0
+        Me.Max_MLB.Text = "XXX"
+        '
+        'Min_MLB
+        '
+        Me.Min_MLB.AutoSize = True
+        Me.Min_MLB.Location = New System.Drawing.Point(37, 59)
+        Me.Min_MLB.Name = "Min_MLB"
+        Me.Min_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Min_MLB.TabIndex = 0
+        Me.Min_MLB.Text = "XXX"
+        '
+        'Rcv_MLB
+        '
+        Me.Rcv_MLB.AutoSize = True
+        Me.Rcv_MLB.Location = New System.Drawing.Point(71, 46)
+        Me.Rcv_MLB.Name = "Rcv_MLB"
+        Me.Rcv_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Rcv_MLB.TabIndex = 0
+        Me.Rcv_MLB.Text = "XXX"
+        '
+        'Lost_MLB
+        '
+        Me.Lost_MLB.AutoSize = True
+        Me.Lost_MLB.Location = New System.Drawing.Point(45, 33)
+        Me.Lost_MLB.Name = "Lost_MLB"
+        Me.Lost_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Lost_MLB.TabIndex = 0
+        Me.Lost_MLB.Text = "XXX"
+        '
+        'Sent_MLB
+        '
+        Me.Sent_MLB.AutoSize = True
+        Me.Sent_MLB.Location = New System.Drawing.Point(45, 20)
+        Me.Sent_MLB.Name = "Sent_MLB"
+        Me.Sent_MLB.Size = New System.Drawing.Size(28, 13)
+        Me.Sent_MLB.TabIndex = 0
+        Me.Sent_MLB.Text = "XXX"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Sent :"
+        '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Export_BT)
         Me.Controls.Add(Me.Icon_PB)
         Me.Controls.Add(Me.Name_LB)
         Me.Controls.Add(Me.Version_LB)
@@ -233,7 +407,6 @@ Partial Class Main
         Me.Controls.Add(Me.Credit)
         Me.Controls.Add(Me.Start_LB)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Stop_BT)
         Me.Controls.Add(Me.Start_BT)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.DataGridView1)
@@ -241,9 +414,12 @@ Partial Class Main
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "PingChecker"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Icon_PB, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -252,7 +428,6 @@ Partial Class Main
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Start_BT As Button
-    Friend WithEvents Stop_BT As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Name_LB As Label
     Friend WithEvents Timer1 As Timer
@@ -265,4 +440,21 @@ Partial Class Main
     Friend WithEvents Started_time_LB As Label
     Friend WithEvents Credit As Label
     Friend WithEvents Version_LB As Label
+    Friend WithEvents Export_BT As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Avg_MLB As Label
+    Friend WithEvents Max_MLB As Label
+    Friend WithEvents Min_MLB As Label
+    Friend WithEvents Rcv_MLB As Label
+    Friend WithEvents Lost_MLB As Label
+    Friend WithEvents Sent_MLB As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Time_MLB As Label
 End Class
--- PingChecker/Main.vb
+++ PingChecker/Main.vb
@@ -1,47 +1,79 @@
 ﻿Public Class Main
     Dim Num As Integer
-    Dim UserDomain
-    Private Sub Start_BT_Click(sender As Object, e As EventArgs) Handles Start_BT.Click
-        If TextBox1.TextLength = 0 Then
-            MsgBox("Please write a domain in the textbox !", MsgBoxStyle.Critical)
-        Else
-            DataGridView1.Rows.Clear()
-            Timer1.Start()
-            Num = 0
-            Started_time_LB.Visible = True
-            Start_LB.Visible = True
-            Started_time_LB.Text = My.Computer.Clock.LocalTime
-            UserDomain = TextBox1.Text
-        End If
-    End Sub
+    Dim UserDomain As String
+    Dim Button As Integer = 0
+    Dim Lost As Integer = 0
+    Dim Rcv As Integer = 0
+    Dim Min As Integer = 0
+    Dim Max As Integer = 0
+    Dim avg1 As Integer = 0
 
-    Private Sub Stop_BT_Click(sender As Object, e As EventArgs) Handles Stop_BT.Click
-        Timer1.Stop()
+    Private Sub Start_BT_Click(sender As Object, e As EventArgs) Handles Start_BT.Click
+        If Button = 0 Then
+            If TextBox1.TextLength = 0 Then
+                MsgBox("Please write a domain in the textbox !", MsgBoxStyle.Critical)
+            Else
+                Lost = 0
+                Rcv = 0
+                Min = 1000
+                Max = 0
+                avg1 = 0
+
+                DataGridView1.Rows.Clear()
+                Timer1.Start()
+                Num = 0
+                Started_time_LB.Visible = True
+                Start_LB.Visible = True
+                Started_time_LB.Text = My.Computer.Clock.LocalTime
+                UserDomain = TextBox1.Text
+                Button = 1
+                Start_BT.Text = "Stop"
+                TextBox1.Enabled = False
+            End If
+        Else
+            Timer1.Stop()
+            Button = 0
+            Start_BT.Text = "Start"
+            TextBox1.Enabled = True
+        End If
+
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim hour = My.Computer.Clock.LocalTime
         Dim Ping As Net.NetworkInformation.PingReply
         Dim SendPing As New Net.NetworkInformation.Ping
-        Dim ResponseTime As Double
+        Dim ResponseTime As Decimal
 
         Try
-            Ping = SendPing.Send(UserDomain)
+            Ping = SendPing.Send(UserDomain, 1000)
             ResponseTime = Ping.RoundtripTime
         Catch ex As Exception
             ResponseTime = 0
         End Try
 
-        If ResponseTime = 0 Then
+        If ResponseTime = 1000 Then
             DataGridView1.Rows.Add(Num.ToString, hour.ToString, TextBox1.Text, "Error")
             DataGridView1.Item(3, Num).Style.BackColor = Color.Red
             DataGridView1.Sort(Time, System.ComponentModel.ListSortDirection.Descending)
+            Lost += 1
 
         Else
             DataGridView1.Rows.Add(Num.ToString, hour.ToString, TextBox1.Text, ResponseTime)
             DataGridView1.Item(3, Num).Style.BackColor = Color.Green
             DataGridView1.Sort(Time, System.ComponentModel.ListSortDirection.Descending)
+            Rcv += 1
+            avg1 += ResponseTime
+            If ResponseTime >= Max Then
+                Max = ResponseTime
+            End If
+
+            If ResponseTime <= Min Then
+                Min = ResponseTime
+            End If
         End If
+
+        Stats()
         Num += 1
     End Sub
 
@@ -57,5 +89,61 @@
 
     Private Sub Name_Click(sender As Object, e As EventArgs) Handles Icon_PB.Click, Name_LB.Click
         Process.Start("https://github.com/ethandudu/PingChecker")
+    End Sub
+
+    Private Sub Export_BT_Click(sender As Object, e As EventArgs) Handles Export_BT.Click
+        SaveFileDialog1.ShowDialog()
+        Dim path As String = SaveFileDialog1.FileName.ToString
+        If path = "" Then
+            MsgBox("Please define the file path", MsgBoxStyle.Exclamation)
+        Else
+            Dim StrExport As String = ""
+            For Each C As DataGridViewColumn In DataGridView1.Columns
+                StrExport &= """" & C.HeaderText & ""","
+            Next
+            StrExport = StrExport.Substring(0, StrExport.Length - 1)
+            StrExport &= Environment.NewLine
+
+            For Each R As DataGridViewRow In DataGridView1.Rows
+                For Each C As DataGridViewCell In R.Cells
+                    If C.Value IsNot Nothing Then
+                        StrExport &= """" & C.Value.ToString & ""","
+                    Else
+                        StrExport &= """" & "" & ""","
+                    End If
+                Next
+                StrExport = StrExport.Substring(0, StrExport.Length - 1)
+                StrExport &= Environment.NewLine
+            Next
+
+            Dim tw As IO.TextWriter = New IO.StreamWriter(path)
+            tw.Write(StrExport)
+            tw.Close()
+        End If
+    End Sub
+
+    Sub Stats()
+        Dim percent As Decimal
+        Dim Sent As Integer = Num + 1
+        Sent_MLB.Text = Sent.ToString
+        Time_MLB.Text = String.Format("{0} s", Sent.ToString)
+
+        'Lost
+        percent = Math.Round((Lost / Sent) * 100)
+        Lost_MLB.Text = String.Format("{0} / {1}%", Lost.ToString, percent)
+
+        'Received
+        percent = Math.Round((Rcv / Sent) * 100)
+        Rcv_MLB.Text = String.Format("{0} / {1}%", Rcv.ToString, percent)
+
+        'Avg
+        Dim Avg As Decimal = Math.Round(avg1 / Rcv)
+        Avg_MLB.Text = String.Format("{0} ms", avg.ToString)
+
+        'Min
+        Min_MLB.Text = String.Format("{0} ms", Min.ToString)
+
+        'Max
+        Max_MLB.Text = String.Format("{0} ms", Max.ToString)
     End Sub
 End Class
